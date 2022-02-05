46854b6130939bf515519330a7fb0897568b1f9a
SandeepMS07 <sandeepms746@gmail.com>
2022-02-06 00:41:21 +0530

    without passing props down manually to each level data can be acessed using react.context

--- src/ReactHooks/useContext/D.jsx
+++ src/ReactHooks/useContext/D.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { employeeContext, studentContext } from './A';
 
 function D(props) {
-  return <div>
-      <h1>D component name : {props.name}</h1>
-  </div>;
+  return <studentContext.Consumer>
+        {(sname)=>
+        {
+            return <employeeContext.Consumer>
+                {(ename)=>
+                {
+                    return <>
+                        <h1>D Component Student : {sname} Employee: {ename}</h1>
+                    </>
+                }}
+            </employeeContext.Consumer>
+        }}
+
+        </studentContext.Consumer>
+  
+//   <div>
+//       <h1>D component name : {props.name}</h1>
+//   </div>;
+
+    
+
 }
 
 export default D;
